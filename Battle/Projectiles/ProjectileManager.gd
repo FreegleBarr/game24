de918dc19a324a3c4107fb5f3dfa8322391a39f8
FreegleBarr <freeglebarr@gmail.com>
2022-02-12 00:00:27 -0300

    integrated art into code

---
+++ Battle/Projectiles/ProjectileManager.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+var BulletPool = []
+var Bullet = preload("res://Battle/Projectiles/Bullet.tscn")
+
+var current = 0
+
+func _ready():
+	for i in range(100):
+		var bullet = Bullet.instance()
+		BulletPool.append(bullet)
+
+func _on_0_spawn_bullet(pos, dir) -> void:
+	var bullet: Bullet = BulletPool[current]
+	var parent: Node = bullet.get_parent()
+	if parent:
+		parent.remove_child(bullet)
+	bullet.position = pos
+	bullet.direction = dir
+	add_child(bullet)
+	current += 1
+	current %= 100
+	
+		
