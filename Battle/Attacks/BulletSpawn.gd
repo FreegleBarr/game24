9aa22ca1ed6a0563ee9eabeae7ff976255f64e9f
FreegleBarr <freeglebarr@gmail.com>
2022-02-12 01:23:53 -0300

    fixed merge problems

--- Battle/Attacks/BulletSpawn.gd
+++ Battle/Attacks/BulletSpawn.gd
@@ -1,11 +1,12 @@
-extends Position2D
+extends BaseAttack
 
 signal spawn_bullet(pos, dir)
 
 var Bullet = preload("res://Battle/Projectiles/Bullet.tscn")
 
+onready var spawn = $SpawnPosition
 
 
 func _on_Timer_timeout() -> void:
 	var direction = Vector2.RIGHT.rotated(randf()*TAU)
-	emit_signal('spawn_bullet', position, direction)
+	emit_signal('spawn_bullet', spawn.position, direction)
