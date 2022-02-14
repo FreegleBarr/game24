1ada2ab2a7ff0ee549142bf5fcdacf131d8ba8f8
FreegleBarr <freeglebarr@gmail.com>
2022-02-13 23:31:48 -0300

    added repeats to wallwalker

--- Battle/Attacks/EyeWallWalker.gd
+++ Battle/Attacks/EyeWallWalker.gd
@@ -1,13 +1,20 @@
 extends BaseAttack
 
 signal spawn_bullet(type, pos, dir)
-onready var playback: AnimationNodeStateMachinePlayback = $AnimationTree.get("parameters/StateMachine/playback")
+onready var timer = $Timer
 
-func _ready() -> void:
-	print(playback.get_current_node())
-
-func start(_args: Array) -> void:
-	var direction = -$Pivot/SpawnPoint.global_position
-	var position = $Pivot/SpawnPoint.global_position
+func start(args: Array) -> void:
+	var direction: Vector2 = -$Pivot/SpawnPoint.global_position
+	var position: Vector2 = $Pivot/SpawnPoint.global_position
 	emit_signal("spawn_bullet", "virus", position, direction)
+	var repeats := int(args[0])
+	var wait_time := float(args[1])
+	for i in range(repeats-1):
+		timer.wait_time = wait_time
+		timer.start()
+		yield(timer, "timeout")
+		position = $Pivot/SpawnPoint.global_position
+		direction = -position
+		emit_signal("spawn_bullet", "virus", position, direction)
+	
 	emit_signal("attack_done")
