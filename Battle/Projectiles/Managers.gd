7e4059e21b62c623622c5043d7fb9956e8c38a0f
FreegleBarr <freeglebarr@gmail.com>
2022-02-17 13:28:59 -0300

    added interact key

--- Battle/Projectiles/Managers.gd
+++ Battle/Projectiles/Managers.gd
@@ -4,9 +4,7 @@ func _ready() -> void:
 	Battle.projectile_manager = self
 
 	
-func _on_spawn_bullet(type, pos, dir) -> void:
-	type = "rocky rock"
-#	print("trying to spawn ", type, "at ", pos)
+func _on_spawn_bullet(type: String, pos: Vector2, dir: Vector2) -> void:
 	if has_node(type.capitalize()):
 		var manager: ProjectileManager = get_node(type.capitalize())
 		manager.spawn_projectile(pos, dir)
