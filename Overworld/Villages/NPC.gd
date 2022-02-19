f8a4439abd9ba4ee8e544175ec1ec2d19b3f94ea
Mallard <mallardbro@hotmail.co.uk>
2022-02-19 03:39:59 +0000

    NPC exported sprite and dialogic array + basic brain layout

--- Overworld/Villages/NPC.gd
+++ Overworld/Villages/NPC.gd
@@ -1,29 +1,56 @@
+tool
 extends Area2D
 
-export var assignedDialogue = ""
+export(Texture) var SpriteTexture setget set_texture
+export(int, 1, 15) var FrameCount := 1 setget set_frame_count
+export(Array, String, "TimelineDropdown") var dialogues = [null,null]
 
-
+var dialogue_index := 0
+var d_node
 var selected := false setget set_selected
 
 var interacting := false setget set_interacting
 
+func set_texture(t:Texture):
+	SpriteTexture = t
+	if $Sprite:
+		$Sprite.texture = t
+	set_frame_count(FrameCount)
+	
+func set_frame_count(i:int):
+	i = max(i,1)
+	FrameCount = i
+	if $Sprite:
+		$Sprite.hframes = FrameCount
+
 func _ready():
 	set_selected(false)
 	set_interacting(false)
+	set_texture(SpriteTexture)
 
+func _on_Dialogic(arg:String):
+	print(name, ": ", arg)
+	dialogue_index += 1
+	if current_dialogue():
+		Dialogic.start(current_dialogue())
 
 func _input(event):
 	if not selected: return
 	if Input.is_action_just_pressed("interact"):
-		set_interacting(not interacting)
+		if interacting or not current_dialogue():
+			return
+		
+		d_node = Dialogic.start(current_dialogue())
+		add_child(d_node)
+		d_node.connect("timeline_end", self, "dialogue_end")
+		d_node.connect("dialogic_signal", self, "_on_Dialogic")
+		
+		set_interacting(true)
 		owner.emit_signal("npc_interaction", interacting)
-	#		print("Hello! My name is ", name)
-		if assignedDialogue != "":
-			var dialogue = Dialogic.start(assignedDialogue)
-			add_child(dialogue)
-			dialogue.connect("timeline_end", self, "dialogue_end")
 
-
+func current_dialogue():
+	if dialogue_index < dialogues.size():
+		return dialogues[dialogue_index]
 
 func dialogue_end(timeline_name):
 	set_interacting(false)
@@ -45,10 +72,11 @@ func set_selected(b:bool):
 
 func select():
 	set_selected(true)
-#	modulate = Color.red
-
 
 func deselect():
 	set_selected(false)
-#	modulate = Color.white
 
+
+func _on_SpriteTimer_timeout():
+	$Sprite.frame = ($Sprite.frame + 1) % $Sprite.hframes
+	pass
