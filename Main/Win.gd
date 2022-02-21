188d196fc84f953ff52ca841f02f3d14f50b4788
FreegleBarr <freeglebarr@gmail.com>
2022-02-21 14:47:32 -0300

    fixed tween in map

---
+++ Main/Win.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+onready var tween := $Tween
+onready var label1 := $CanvasLayer/CenterContainer/PanelContainer/VBoxContainer/Label
+onready var label2 := $CanvasLayer/CenterContainer/PanelContainer/VBoxContainer/Label2
+
+func _ready() -> void:
+	tween.interpolate_property(label1, "percent_visible",
+		0, 1, label1.text.length()*0.1)
+	tween.start()
+
+
+func _on_Tween_tween_completed(object: Object, key: NodePath) -> void:
+	if object == label1:
+		tween.interpolate_property(label2, "percent_visible",
+			0, 1, label2.text.length()*0.1)
+		tween.start()
--- Map/Map.gd
+++ Map/Map.gd
@@ -5,3 +5,18 @@ export var toggleabble := true
 func _input(event):
 	if toggleabble and Input.is_action_just_pressed("map"):
 		visible = !visible
+
+
+func _on_Brain_mouse_entered() -> void:
+	var tween = $brain/Tween
+	tween.stop_all()
+	tween.interpolate_property($brain, 'modulate',
+		$brain.modulate, Color(1,38.0/255,167.0/255, 1), 0.5, Tween.TRANS_CIRC, Tween.EASE_IN_OUT)
+	tween.start()
+
+func _on_Brain_mouse_exited() -> void:
+	var tween = $brain/Tween
+	tween.stop_all()	
+	tween.interpolate_property($brain, 'modulate',
+	Color(1,38.0/255,167.0/255, 1), Color(1,130.0/255, 160.0/255, 1), 0.2, Tween.TRANS_CIRC, Tween.EASE_IN_OUT)
+	tween.start()
